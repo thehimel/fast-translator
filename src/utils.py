109bdfa3d084ad60f61664aeca99005afbcf060c
thehimel <60639301+thehimel@users.noreply.github.com>
2024-08-20 20:53:40 +0600

    use gpu if available as device in pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,12 @@
+import torch
 from transformers import pipeline
 
 
 async def translator(text: str, source: str, target: str) -> str:
-    pipe = pipeline("translation", model="facebook/nllb-200-distilled-600M")
+
+    model = "facebook/nllb-200-distilled-600M"
+    device = 0 if torch.cuda.is_available() else -1  # Use GPU if available
+
+    pipe = pipeline("translation", model=model, device=device)
     translation = pipe(text, src_lang=source, tgt_lang=target)
     return translation[0]["translation_text"]
